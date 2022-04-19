7e9adc546bb7737957fc589db825f2de98d2b009
Chardine Clement <clement.chardine@student-cs.fr>
2022-04-19 17:23:22 +0900

    truc

--- exec.cmd
+++ exec.cmd
@@ -1,3 +1,4 @@
+curl --output front.py --url https://raw.githubusercontent.com/Eyusd/smallandsimple/main/front.py
 pip install numpy --user
 pip install pysimplegui --user
 pip install pandas --user
